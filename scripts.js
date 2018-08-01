7c0943004bf7cab27eb8645b989c8d713ad14441
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2018-08-01 00:39:33 -0500

    Added listeners to show/hide the div id when the div id is hovered upon

--- scripts.js
+++ scripts.js
@@ -13,8 +13,14 @@ document.addEventListener("DOMContentLoaded", function () {
         div.className = "divSquare";
         flexContainer.appendChild(div);
         div.id = document.getElementsByClassName("divSquare").length;
+
+        div.addEventListener("mouseover", function () {
+            let idText = document.createTextNode(div.id.toString());
+            div.appendChild(idText);
+        });
+
+        div.addEventListener("mouseout", function () {
+            div.removeChild(div.firstChild);
+        });
     });
-
-
-
 });
