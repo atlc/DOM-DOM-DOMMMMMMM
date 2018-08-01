0c54bd045c41d1f2e323afa1b7da33f90ae34dca
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2018-08-01 11:50:48 -0500

    Added events on clicking to change element color, and delete at certain indices

--- scripts.js
+++ scripts.js
@@ -8,19 +8,54 @@ document.addEventListener("DOMContentLoaded", function () {
     flexContainer.className = "flex-container";
     document.body.appendChild(flexContainer);
 
+    function getRandomColor() {
+        let r = Math.floor((Math.random() * 255) + 1)
+        let g = Math.floor((Math.random() * 255) + 1)
+        let b = Math.floor((Math.random() * 255) + 1)
+        let colorStr = "rgb(" + r + ", " + g + ", " + b +")"
+        return colorStr;
+    }
+
     btn.addEventListener("click", function () {
         let div = document.createElement("div");
         div.className = "divSquare";
         flexContainer.appendChild(div);
         div.id = document.getElementsByClassName("divSquare").length;
+        attachDivEventListeners(div);
+    });
 
+    function attachDivEventListeners(div) {
         div.addEventListener("mouseover", function () {
             let idText = document.createTextNode(div.id.toString());
             div.appendChild(idText);
         });
-
+    
         div.addEventListener("mouseout", function () {
             div.removeChild(div.firstChild);
         });
-    });
+    
+        div.addEventListener("click", function () {
+            div.style.backgroundColor = getRandomColor();
+        });
+
+        div.addEventListener("dblclick", function () {
+            let len = document.getElementsByClassName("divSquare").length;
+            let nextSquare = document.getElementById(div.id).nextElementSibling;
+            let previousSquare = document.getElementById(div.id).previousElementSibling;
+
+            if (div.id % 2 == 0) {
+                if (div.id >= len) {
+                    alert("There are no boxes after this one!");
+                } else {
+                    nextSquare.parentNode.removeChild(nextSquare); // https://stackoverflow.com/questions/3387427/remove-element-by-id#comment3521845_3387427
+                }
+            } else {
+                if (previousSquare == null) {
+                    alert("There are no boxes before this one!");
+                } else {
+                    previousSquare.parentNode.removeChild(previousSquare);
+                }
+            }   
+        });
+    };
 });
